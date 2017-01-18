9f6df436394630095d729900c2fca274e5d2ef7a
Dan Woodward <dan.woodward190@gmail.com>
2017-01-18 09:18:36 +0000

    appc-cli

--- GruntFile.js
+++ GruntFile.js
@@ -9,7 +9,7 @@ module.exports = function( grunt )
         settings:
         {
             ppUuid:             "15717f03-7b6b-4885-ae42-07493750acc4",
-            distributionName:   "Git Test"
+            distributionName:   "iPhone Developer: Jenkins Bot (LLKM4W48ZV)"
         },
 
         clean:
@@ -24,70 +24,38 @@ module.exports = function( grunt )
                 src: [ "dist/coverage" ]
             }
         },
-
-        appc_cli:
+		'dist-prod': {
+            'command': "run",
+            'args': [ 
+                '-p', 'ios',
+                '-T', 'dist-adhoc',
+                '-R', '<%= settings.distributionName %>',
+                '-P', '<%= settings.ppUuid %>',
+                '-O', './dist/prod/',
+                '--log-level', 'trace'
+            ]
+        },
+     	clean:
         {
-            options:{},
-            ios_adhoc: {
-                options:
-                {
-                	command:"ti",
-                    subcommand:"build",
-                    projectDir: ".",
-                    platform:   "ios",
-                    target:     "dist-adhoc",
-                    buildOnly:  true,
-                    distributionName: "<%= settings.distributionName %>",
-                    ppUuid: "<%= settings.ppUuid %>",
-                    outputDir: "./dist/artifacts"
-                }
-            },
-            ios_store: {
-                options:
-                {
-                    command:    "build",
-                    projectDir: ".",
-                    platform:   "ios",
-                    target:     "dist-appstore",
-                    buildOnly:  true,
-                    distributionName: "<%= settings.distributionName %>",
-                    ppUuid: "<%= settings.ppUuid %>",
-                    outputDir: "./dist/artifacts"
-                }
-            },
-            dev_ios:
+            options:
             {
-                options:
-                {
-                    command:    "build",
-                    args:       "--shadow",
-                    projectDir: ".",
-                    platform:   "ios",
-                    buildOnly:  false,
-                }
-            },
-            clean:
-            {
-                options:
-                {
-                    command:    "clean",
-                    projectDir: "."
-                }
+                command:    "clean",
+                projectDir: "."
             }
         }
     } );
 
     // Load the required plug-ins
     //
-    grunt.loadNpmTasks('grunt-appc-cli');
+    grunt.loadNpmTasks( "grunt-appc-cli"		);
     grunt.loadNpmTasks( "grunt-alloy"           );
     grunt.loadNpmTasks( "grunt-contrib-clean"   );
     grunt.loadNpmTasks( "grunt-shell"           );
 
     // Default task(s)
     //
-    grunt.registerTask( "default",      [ "appc_cli:ios_adhoc"] );
+    grunt.registerTask( "default",      [ "dist-prod","clean"] );
     grunt.registerTask( "coverage",     [] );
-    grunt.registerTask( "ios-adhoc",    [ "appc_cli:ios_adhoc"      ] );
-    grunt.registerTask( "ios",          [ "appc_cli:dev_ios"        ] );
+    grunt.registerTask( "ios-adhoc",    [ "appc-cli:ios_adhoc"      ] );
+    grunt.registerTask( "ios",          [ "appc-cli:dev_ios"        ] );
 };
